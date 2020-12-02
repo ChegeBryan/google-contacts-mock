cb2099c904c47431593afe55fe99fb47467ecb90
Chege Bryan <cbbryan8@gmail.com>
2020-12-02 20:28:56 +0300

    Add centered contact avatar

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Contact extends StatelessWidget {
@@ -7,12 +9,45 @@ class Contact extends StatelessWidget {
       appBar: AppBar(
         title: Text('Contact'),
       ),
+      body: ContactDetails(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(
           Icons.edit_outlined,
         ),
       ),
+    );
+  }
+}
+
+class ContactDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 220,
+          //width: 200,
+          child: FractionallySizedBox(
+            heightFactor: 0.4,
+            child: Center(
+              child: CircleAvatar(
+                backgroundColor:
+                    Colors.primaries[Random().nextInt(Colors.primaries.length)],
+                child: Text(
+                  "A",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                  ),
+                ),
+                radius: 40.0,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
