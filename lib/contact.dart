3314e2a6601652c8cf520e3d7afae2ea01982386
Chege Bryan <cbbryan8@gmail.com>
2020-12-03 11:37:51 +0300

    Enable scroll on contact details

--- lib/contact.dart
+++ lib/contact.dart
@@ -55,93 +55,97 @@ class ContactDetails extends StatelessWidget {
       ),
     );
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return ListView(
       children: [
-        Container(
-          height: 220,
-          //width: 200,
-          child: FractionallySizedBox(
-            heightFactor: 0.4,
-            child: Center(
-              child: CircleAvatar(
-                backgroundColor:
-                    Colors.primaries[Random().nextInt(Colors.primaries.length)],
-                child: Text(
-                  "A",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 40.0,
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 220,
+              //width: 200,
+              child: FractionallySizedBox(
+                heightFactor: 0.4,
+                child: Center(
+                  child: CircleAvatar(
+                    backgroundColor: Colors
+                        .primaries[Random().nextInt(Colors.primaries.length)],
+                    child: Text(
+                      "A",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40.0,
+                      ),
+                    ),
+                    radius: 40.0,
                   ),
                 ),
-                radius: 40.0,
               ),
             ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 20.0, bottom: 20.0),
-          child: Text(
-            'Contact Name',
-            style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w500),
-          ),
-        ),
-        Divider(),
-        buttonSection,
-        Divider(),
-        ListTile(
-          leading: Icon(
-            Icons.call_outlined,
-            color: color,
-          ),
-          title: Text('+25400000000'),
-          subtitle: Text('Mobile'),
-          trailing: Wrap(
-            spacing: 24,
-            children: [
-              Icon(
-                Icons.videocam_outlined,
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, bottom: 20.0),
+              child: Text(
+                'Contact Name',
+                style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Divider(),
+            buttonSection,
+            Divider(),
+            ListTile(
+              leading: Icon(
+                Icons.call_outlined,
                 color: color,
               ),
-              Icon(
-                Icons.message_outlined,
-                color: color,
+              title: Text('+25400000000'),
+              subtitle: Text('Mobile'),
+              trailing: Wrap(
+                spacing: 24,
+                children: [
+                  Icon(
+                    Icons.videocam_outlined,
+                    color: color,
+                  ),
+                  Icon(
+                    Icons.message_outlined,
+                    color: color,
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            Divider(
+              indent: 56.0,
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.email_outlined,
+                color: Colors.grey,
+              ),
+              title: Text('email@test.com'),
+              subtitle: Text('Home · Default'),
+            ),
+            Divider(
+              indent: 56.0,
+            ),
+            ListTile(
+              leading: Icon(
+                CustomIcons.whatsapp,
+                color: Colors.green,
+              ),
+              title: Text('Video call +254000000'),
+            ),
+            Divider(
+              indent: 56.0,
+            ),
+            ListTile(
+              leading: Icon(
+                CustomIcons.whatsapp,
+                color: Colors.green,
+              ),
+              title: Text('Message call +254000000'),
+            ),
+            Divider(),
+          ],
         ),
-        Divider(
-          indent: 56.0,
-        ),
-        ListTile(
-          leading: Icon(
-            Icons.email_outlined,
-            color: Colors.grey,
-          ),
-          title: Text('email@test.com'),
-          subtitle: Text('Home · Default'),
-        ),
-        Divider(
-          indent: 56.0,
-        ),
-        ListTile(
-          leading: Icon(
-            CustomIcons.whatsapp,
-            color: Colors.green,
-          ),
-          title: Text('Video call +254000000'),
-        ),
-        Divider(
-          indent: 56.0,
-        ),
-        ListTile(
-          leading: Icon(
-            CustomIcons.whatsapp,
-            color: Colors.green,
-          ),
-          title: Text('Message call +254000000'),
-        ),
-        Divider(),
       ],
     );
   }
