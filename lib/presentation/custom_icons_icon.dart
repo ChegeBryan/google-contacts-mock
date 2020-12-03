863fd4596e83acabfb052c5e6d91f6542f7d1c74
Chege Bryan <cbbryan8@gmail.com>
2020-12-03 11:02:50 +0300

    Add whatsapp icon

---
+++ lib/presentation/custom_icons_icon.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const _kFontFam = 'Whatsapp';
+  static const _kFontPkg = null;
+
+  static const IconData whatsapp =
+      IconData(0xf232, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
