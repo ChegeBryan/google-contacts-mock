99bfd41745a4bcb5b30e9e0d38be53de72492810
Chege Bryan <cbbryan8@gmail.com>
2020-12-04 10:54:06 +0300

    Separate icon families

--- lib/presentation/custom_icons_icon.dart
+++ lib/presentation/custom_icons_icon.dart
@@ -3,11 +3,10 @@ import 'package:flutter/widgets.dart';
 class CustomIcons {
   CustomIcons._();
 
-  static const _kFontFam = 'Customicon';
   static const _kFontPkg = null;
 
   static const IconData whatsapp =
-      IconData(0xf232, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xf232, fontFamily: "Whatsapp", fontPackage: _kFontPkg);
   static const IconData magic =
-      IconData(0xf0d0, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xf0d0, fontFamily: "Magic", fontPackage: _kFontPkg);
 }
