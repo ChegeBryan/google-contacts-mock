52aa78db535afe9796ececcce2ff4569f31e4012
Chege Bryan <cbbryan8@gmail.com>
2020-12-04 10:35:58 +0300

    Register magic wand icondata

--- lib/presentation/custom_icons_icon.dart
+++ lib/presentation/custom_icons_icon.dart
@@ -3,9 +3,11 @@ import 'package:flutter/widgets.dart';
 class CustomIcons {
   CustomIcons._();
 
-  static const _kFontFam = 'Whatsapp';
+  static const _kFontFam = 'Customicon';
   static const _kFontPkg = null;
 
   static const IconData whatsapp =
       IconData(0xf232, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData magic =
+      IconData(0xf0d0, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
