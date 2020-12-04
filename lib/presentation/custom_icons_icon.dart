caaadc5990b5e8a54b77aaba272a7b88702bd4e0
Chege Bryan <cbbryan8@gmail.com>
2020-12-04 11:33:48 +0300

    Merge custom fonts

--- lib/presentation/custom_icons_icon.dart
+++ lib/presentation/custom_icons_icon.dart
@@ -3,10 +3,11 @@ import 'package:flutter/widgets.dart';
 class CustomIcons {
   CustomIcons._();
 
+  static const _kFontFam = 'MyFlutterApp';
   static const _kFontPkg = null;
 
-  static const IconData whatsapp =
-      IconData(0xf232, fontFamily: "Whatsapp", fontPackage: _kFontPkg);
   static const IconData magic =
-      IconData(0xf0d0, fontFamily: "Magic", fontPackage: _kFontPkg);
+      IconData(0xf0d0, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData whatsapp =
+      IconData(0xf232, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
