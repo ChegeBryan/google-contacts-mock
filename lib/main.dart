38b885769c05cd4b4df34ed0b5efed23b6af5f4b
Chege Bryan <cbbryan8@gmail.com>
2020-12-04 10:47:44 +0300

    Add contacts and merge list tiles

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:floating_search_bar/floating_search_bar.dart';
 import 'package:google_contacts/contact.dart';
+import 'package:google_contacts/presentation/custom_icons_icon.dart';
 
 void main() {
   runApp(MyApp());
@@ -85,6 +86,17 @@ class MyApp extends StatelessWidget {
                     ],
                   ),
                 ),
+              ),
+              ListTile(
+                leading: Icon(Icons.person_outline),
+                title: Text("Contacts"),
+                trailing: Text("333"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(CustomIcons.magic),
+                title: Text("Merge & fix"),
+                onTap: () {},
               ),
             ],
           ),
