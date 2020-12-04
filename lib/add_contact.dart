e2b92ae6bc43c3876ae5bf9fafb944766999ec07
Chege Bryan <cbbryan8@gmail.com>
2020-12-04 15:32:52 +0300

    Add styling to appbar

--- lib/add_contact.dart
+++ lib/add_contact.dart
@@ -5,7 +5,17 @@ class AddContact extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Create contact"),
+        title: Text(
+          "Create contact",
+          style: TextStyle(
+            color: Colors.grey[800],
+          ),
+        ),
+        elevation: 0,
+        iconTheme: IconThemeData(
+          color: Colors.grey[700],
+        ),
+        backgroundColor: Colors.grey[50],
       ),
     );
   }
