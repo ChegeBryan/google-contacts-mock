1516644b4ad7218365e90601ac8e0389edbc74c0
Chege Bryan <cbbryan8@gmail.com>
2020-12-04 13:26:40 +0300

    Wrap floatingsearchbar in scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -46,105 +46,107 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FloatingSearchBar.builder(
-      itemBuilder: (BuildContext context, _) {
-        return ListTile(
-          leading: CircleAvatar(
-            // use random colors from the primary colors
-            backgroundColor:
-                Colors.primaries[Random().nextInt(Colors.primaries.length)],
-            child: Text(
-              "AB",
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-          ),
-          title: Text("Contact Name"),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Contact()),
-            );
-          },
-        );
-      },
-      pinned: true,
-      padding: EdgeInsets.only(top: 10.0),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            Container(
-              height: 110.0,
-              child: DrawerHeader(
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      maxRadius: 16.0,
-                      child: Icon(Icons.person),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Text(
-                        'Contacts Mock',
-                        style: TextStyle(
-                          color: Colors.grey[700],
-                          fontSize: 24,
-                        ),
-                      ),
-                    ),
-                  ],
+    return Scaffold(
+      body: FloatingSearchBar.builder(
+        itemBuilder: (BuildContext context, _) {
+          return ListTile(
+            leading: CircleAvatar(
+              // use random colors from the primary colors
+              backgroundColor:
+                  Colors.primaries[Random().nextInt(Colors.primaries.length)],
+              child: Text(
+                "AB",
+                style: TextStyle(
+                  color: Colors.white,
                 ),
               ),
             ),
-            ListTile(
-              leading: Icon(Icons.person_outline),
-              title: Text("Contacts"),
-              trailing: Text("333"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.sync),
-              title: Text("Merge & fix"),
-              onTap: () {},
-            ),
-            Divider(),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0, left: 8.0),
-              child: Text("Labels"),
-            ),
-            ListTile(
-              leading: Icon(Icons.label_outline),
-              title: Text("Family"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.add),
-              title: Text("Create Label"),
-              onTap: () {},
-            ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.settings_outlined),
-              title: Text("Settings"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.help_outline),
-              title: Text("Help and Support"),
-              onTap: () {},
-            ),
-          ],
+            title: Text("Contact Name"),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Contact()),
+              );
+            },
+          );
+        },
+        pinned: true,
+        padding: EdgeInsets.only(top: 10.0),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              Container(
+                height: 110.0,
+                child: DrawerHeader(
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        maxRadius: 16.0,
+                        child: Icon(Icons.person),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          'Contacts Mock',
+                          style: TextStyle(
+                            color: Colors.grey[700],
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.person_outline),
+                title: Text("Contacts"),
+                trailing: Text("333"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.sync),
+                title: Text("Merge & fix"),
+                onTap: () {},
+              ),
+              Divider(),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, left: 8.0),
+                child: Text("Labels"),
+              ),
+              ListTile(
+                leading: Icon(Icons.label_outline),
+                title: Text("Family"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.add),
+                title: Text("Create Label"),
+                onTap: () {},
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.settings_outlined),
+                title: Text("Settings"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.help_outline),
+                title: Text("Help and Support"),
+                onTap: () {},
+              ),
+            ],
+          ),
         ),
-      ),
-      trailing: CircleAvatar(
-        child: Text("CB"),
-      ),
-      onChanged: (String value) {},
-      onTap: () {},
-      decoration: InputDecoration.collapsed(
-        hintText: "Search...",
+        trailing: CircleAvatar(
+          child: Text("CB"),
+        ),
+        onChanged: (String value) {},
+        onTap: () {},
+        decoration: InputDecoration.collapsed(
+          hintText: "Search...",
+        ),
       ),
     );
   }
