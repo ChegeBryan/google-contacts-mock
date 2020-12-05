84e0193c4a5e7232fcae5f051a25d8bd6f3b5776
Chege Bryan <cbbryan8@gmail.com>
2020-12-05 15:55:32 +0300

    Add datepicker initial class

---
+++ lib/ui/partials/date_picker_mod.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyDatePickerInput extends StatefulWidget {
+  @override
+  _MyDatePickerInputState createState() => _MyDatePickerInputState();
+}
+
+class _MyDatePickerInputState extends State<MyDatePickerInput> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onTap: () {},
+      initialValue: '23/04/1998',
+      decoration: InputDecoration(
+        icon: Icon(Icons.event),
+        labelText: "Date",
+        suffixIcon: Icon(Icons.keyboard_arrow_down),
+        border: OutlineInputBorder(),
+      ),
+    );
+  }
+}
