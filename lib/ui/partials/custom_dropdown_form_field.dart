28d9c6b77fbfba7ac629a6bde8936d5867f8816e
Chege Bryan <cbbryan8@gmail.com>
2020-12-07 11:34:57 +0300

    Add phone field drop down items

--- lib/ui/partials/custom_dropdown_form_field.dart
+++ lib/ui/partials/custom_dropdown_form_field.dart
@@ -7,9 +7,22 @@ class CustomButtonFormField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int _dropdownValue = 1;
+
     return Padding(
       padding: EdgeInsets.only(left: 56.0, top: 8.0, right: 130.0),
       child: DropdownButtonFormField(
+        value: _dropdownValue,
+        items: [
+          DropdownMenuItem(child: Text("Mobile"), value: 1),
+          DropdownMenuItem(child: Text("Home"), value: 2),
+          DropdownMenuItem(child: Text("Work"), value: 3),
+        ],
+        onChanged: (value) {
+          setState(() {
+            _dropdownValue = value;
+          });
+        },
         decoration: InputDecoration(
           labelText: "Label",
           floatingLabelBehavior: FloatingLabelBehavior.always,
